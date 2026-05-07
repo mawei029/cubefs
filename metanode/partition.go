5e2f121f4473ebe07f80af7f3ee15aa5257d213c
Victor1319 <zengxuewei@oppo.com>
2026-05-07 12:07:14 +0000

    fix(metanode): streamline Clear method and adjust uniqChecker logic. #1000939416
    - Removed unnecessary check for RocksDB store in the Clear method to simplify the code.
    - Reset leaseApplyTime in the Clear method to ensure proper state management.
    - Updated uniqChecker's legalIn method to always return false, altering its validation logic.
    
    Signed-off-by: Victor1319 <zengxuewei@oppo.com>

--- metanode/partition.go
+++ metanode/partition.go
@@ -2121,10 +2121,6 @@ func (mp *metaPartition) LoadDataFromRocksDb() (err error) {
 func (mp *metaPartition) Clear() (err error) {
 	log.LogDebugf("[Clear] mp(%v) clear data", mp.config.PartitionId)
 
-	if !mp.HasRocksDBStore() {
-		return nil
-	}
-
 	handle, err := mp.inodeTree.CreateBatchWriteHandle()
 	if err != nil {
 		log.LogErrorf("[Clear] mp(%v) failed to open write handle, err(%v)", mp.config.PartitionId, err)
@@ -2175,6 +2171,7 @@ func (mp *metaPartition) Clear() (err error) {
 	mp.txProcessor.txManager.txIdAlloc.setTransactionID(0)
 	mp.config.Cursor = 0
 	mp.config.UniqId = 0
+	mp.leaseApplyTime = 0
 	return
 }
 
